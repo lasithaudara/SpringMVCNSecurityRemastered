2272d5c9bc8205f453cde82e9caf758d0e293a51
lasithaup <lasithaup@gmail.com>
2023-12-18 17:08:27 +0530

    add configuration

--- src/main/java/com/lasitha/practice/config/AppConfig.java
+++ src/main/java/com/lasitha/practice/config/AppConfig.java
@@ -1,4 +1,22 @@
 package com.lasitha.practice.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.lasitha.practice")
 public class AppConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+            viewResolver.setPrefix("/WEB-INF/views/");
+            viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 }
